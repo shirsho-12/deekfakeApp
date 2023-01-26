9ff24b4aac5f77dec70a0ce194ab67dcefce660a
Shirshajit Sen Gupta <shirshajit@gmail.com>
2023-01-27 00:31:38 +0800

    Flask API: Image Generation

--- lib/widgets/output_window.dart
+++ lib/widgets/output_window.dart
@@ -1,8 +1,11 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:file_picker_cross/file_picker_cross.dart';
 import 'package:flutter/material.dart';
+import 'package:gan_deepfake/services/flask_api.dart';
 import 'package:gan_deepfake/shared.dart';
+import 'dart:developer' as devtools show log;
 
 class OutputWidget extends StatefulWidget {
   const OutputWidget({Key? key}) : super(key: key);
@@ -12,6 +15,8 @@ class OutputWidget extends StatefulWidget {
 }
 
 class _OutputWidgetState extends State<OutputWidget> {
+  bool isGenerating = false;
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -24,18 +29,24 @@ class _OutputWidgetState extends State<OutputWidget> {
                 children: [
                   ElevatedButton(
                     onPressed: () async {
-                      //! TODO: Generate Image -> MethodChannel
+                      // Generate Image -> MethodChannel
+                      devtools.log("Generate Image", name: "OutputWidget");
+                      // final response =
+                      //     await StarGanModel.generateImage().then((value) {
+                      //   Map<String, dynamic> data = jsonDecode(value);
+                      //   if (data['status'] == 'ok') {
+                      //     return data["filename"];
+                      //   }
+                      //   return "sample.jpg";
+                      // });
                       setState(() {
-                        outputPath = SharedData.referencePath;
-                        print(outputPath);
-                        // outputFile = await FilePickerCross.fromInternalPath(
-                        //   path: outputPath,
-                        // );
-                        File file = File(outputPath);
-                        FilePickerCross outputFile =
-                            FilePickerCross(file.readAsBytesSync());
-                        outputFile.saveToPath(path: "temp/output.png");
-                        SharedData.setOutputPath(outputPath);
+                        // outputPath = "assets/sample.jpg";
+                        isGenerating = true;
+                        // File file = File(outputPath);
+                        // FilePickerCross outputFile =
+                        // FilePickerCross(file.readAsBytesSync());
+                        // outputFile.saveToPath(path: "temp/output.png");
+                        // SharedData.setOutputPath(outputPath);
                       });
                     },
                     child: const Text(
@@ -44,9 +55,36 @@ class _OutputWidgetState extends State<OutputWidget> {
                   ),
                   SizedBox(
                     width: width,
-                    height: height * 0.36,
-                    child: outputPath != "" && File(outputPath).existsSync()
-                        ? Image.file(File(outputPath))
+                    height: height * 0.34,
+                    child: isGenerating
+                        ? FutureBuilder<String>(
+                            future: StarGanModel.generateImage(),
+                            builder: (context, snapshot) {
+                              switch (snapshot.connectionState) {
+                                case ConnectionState.none:
+                                  return Container();
+                                case ConnectionState.active:
+                                case ConnectionState.waiting:
+                                  return const CircularProgressIndicator();
+                                case ConnectionState.done:
+                                  if (snapshot.hasError) {
+                                    return Text('Error: ${snapshot.error}');
+                                  }
+                                  Map<String, dynamic> data =
+                                      jsonDecode(snapshot.data!);
+                                  if (data['status'] == 'ok') {
+                                    outputPath = 'assets/${data["filename"]}';
+                                    File file = File(outputPath);
+                                    FilePickerCross outputFile =
+                                        FilePickerCross(file.readAsBytesSync());
+                                    outputFile.saveToPath(
+                                        path: "temp/output.png");
+                                    devtools.log(outputPath);
+                                    return Image.file(File(outputPath));
+                                  }
+                                  return Text('Result: ${snapshot.data}');
+                              }
+                            })
                         : Container(),
                   ),
                   SizedBox(height: height * 0.01),
